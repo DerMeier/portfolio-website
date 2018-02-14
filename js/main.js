a8585f6fabced470d3237da6c5d3e400018e537e
Praktikant <kimhep@gmx.de>
2018-02-14 17:50:40 +0100

    Namen der Filter Klassen geändert von 1,2,3,4 etc auf bnw ( black n white) psm (photoshop manipulation) ccd (charcoal drawing)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-function addiere(a, b) {
-    console.log(a + b);
-}
 
-function versteckeAlleBilderDieNichtBWSind() {
-    $('.portfolio-item:not(.1)').hide();
-    $('.portfolio-item.1').show();
-}
+//$('.portfolio-item').data('filter', 'schwarzweiss').css('outline', '3px solid pink');
 
 function zeigeAlleBilder() {
     $('.portfolio-item').show();
 }
 
-function versteckeAlleBilderDieNichtManipulateSind() {
-    $('.portfolio-item:not(.2)').hide();
-    $('.portfolio-item.2').show();
+function FilterAlleSchwarzWeissBilder() {
+    //$('.portfolio-item').data('filter', 'schwarzweiss').hide();
+    $('.portfolio-item:not(.bnw)').hide();
+    $('.portfolio-item.bnw').show();
+}
+
+function FilterAllePsManipulationBilder() {
+    $('.portfolio-item:not(.psm)').hide();
+    $('.portfolio-item.psm').show();
+}
+
+function FilterAlleKohleBilder() {
+    $('.portfolio-item:not(.ccd)').hide();
+    $('.portfolio-item.ccd').show();
 }
