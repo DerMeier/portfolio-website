538a8a9fa6423ef7e23c8e4263d63643e7320984
Praktikant <kimhep@gmx.de>
2018-03-08 10:19:32 +0100

    -Masonry an die Filterbuttons gehängt -Aktiver Button wird nun visuell angezeigt

--- js/main.js
+++ js/main.js
@@ -3,20 +3,56 @@
 
 function zeigeAlleBilder() {
     $('.portfolio-item').show();
+    $('.portfolio-list').masonry();
+}
+
+function FilterAlleKohleBilder() {
+    $('.portfolio-item:not(.ccd)').hide();
+    $('.portfolio-item.ccd').show();
+    $('.portfolio-list').masonry({
+
+        itemSelector:'.ccd'
+    });
 }
 
 function FilterAlleSchwarzWeissBilder() {
     //$('.portfolio-item').data('filter', 'schwarzweiss').hide();
     $('.portfolio-item:not(.bnw)').hide();
     $('.portfolio-item.bnw').show();
+    $('.portfolio-list').masonry({
+
+        itemSelector:'.bnw'
+    });
 }
 
 function FilterAllePsManipulationBilder() {
     $('.portfolio-item:not(.psm)').hide();
     $('.portfolio-item.psm').show();
+    $('.portfolio-list').masonry({
+
+        itemSelector:'.psm'
+    });
 }
 
-function FilterAlleKohleBilder() {
-    $('.portfolio-item:not(.ccd)').hide();
-    $('.portfolio-item.ccd').show();
+function FilterAlleLogoBilder() {
+    $('.portfolio-item:not(.logo)').hide();
+    $('.portfolio-item.logo').show();
+    $('.portfolio-list').masonry({
+
+        itemSelector:'.logo'
+    });
 }
+
+function FilterAllePlatzhalter() {
+    $('.portfolio-item:not(.platzhalter)').hide();
+    $('.portfolio-item.platzhalter').show();
+    $('.portfolio-list').masonry({
+
+        itemSelector:'.platzhalter'
+    });
+}
+
+$('button').on('click', function(){
+    $('button').removeClass('active')
+    $(this).addClass('active');
+});
