103e1760b3d7cc1eeb343b163ad50623653e0e24
Praktikant <kimhep@gmx.de>
2018-03-08 16:52:36 +0100

    -Portfolio-Filter "verbessert" (mit data-filter gearbeitet) -Klassen namen überarbeitet -erste Icons für die Filter eingebaut

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 //$('.portfolio-item').data('filter', 'schwarzweiss').css('outline', '3px solid pink');
 
-function zeigeAlleBilder() {
+/*function zeigeAlleBilder() {
     $('.portfolio-item').show();
     $('.portfolio-list').masonry();
 }
@@ -50,9 +50,40 @@ function FilterAllePlatzhalter() {
 
         itemSelector:'.platzhalter'
     });
-}
+}*/
 
 $('button').on('click', function(){
     $('button').removeClass('active')
     $(this).addClass('active');
 });
+
+$('.filter-icons').on('click', function(){
+    $('.filter-icons').removeClass('active-icon')
+    $(this).addClass('active-icon');
+});
+
+(function ($) {
+
+
+    var $grid = $('.portfolio-list');
+    var $gridItem = $('.portfolio-item');
+
+    $grid.masonry({
+        itemSelector: '.portfolio-item'
+    });
+
+    $('button').click(function () {
+        var filter = $(this).data('filter');
+
+        if (filter === 'all') {
+            $gridItem.appendTo($grid);
+        }
+        else {
+            $gridItem.appendTo($grid)
+                .css('top','')
+                .not('[data-filter*="' + filter + '"]')
+                .remove();
+        }
+        $grid.masonry("reloadItems").masonry();
+    })
+}(jQuery));
